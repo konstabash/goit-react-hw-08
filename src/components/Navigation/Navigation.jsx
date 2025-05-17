473273912075b32a4fa912059eb3b87ba99c48c9
Konstantin <bashuk.kostya@gmail.com>
2025-05-17 23:18:23 +0300

    hw8.4

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,15 +4,18 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div className={s.container}>
       <NavLink className={s.link} to="/">
         Home
       </NavLink>
-
-      <NavLink className={s.link} to="/contacts">
-        Phonebook
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={s.link} to="/contacts">
+          Phonebook
+        </NavLink>
+      )}
     </div>
   );
 };
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,107 +1,11 @@
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { Field, Formik, Form } from "formik";
-import { useDispatch } from "react-redux";
-import * as Yup from "yup";
-import { registerThunk } from "../../redux/auth/operations";
-import { Link } from "react-router";
+import { Container } from "@mui/material";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 
 const RegistrationPage = () => {
-  const dispatch = useDispatch();
-
-  const initialValues = {
-    name: "",
-    email: "",
-    password: "",
-  };
-
-  const handleSubmit = (values, actions) => {
-    dispatch(registerThunk(values));
-    console.log(values);
-    actions.resetForm();
-  };
-
-  const FeedbackSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(2, "Too Short!")
-      .max(60, "Too Long!")
-      .required("Required"),
-    email: Yup.string().email("Must be a valid email!").required("Required"),
-    password: Yup.string()
-      .min(8, "Too short")
-      .max(64, "Too long")
-      .required("Required"),
-  });
-
   return (
     <>
       <Container component="main" maxWidth="xs">
-        <Box marginTop="64px">
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
-            validationSchema={FeedbackSchema}
-          >
-            <Form>
-              <Box>
-                <Field
-                  as={TextField}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="name"
-                  type="text"
-                  label="Full Name"
-                  name="name"
-                  autoFocus
-                />
-                <Field
-                  as={TextField}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  type="email"
-                  label="Email Address"
-                  name="email"
-                  autoFocus
-                />
-                <Field
-                  as={TextField}
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign Up
-                </Button>
-                <Grid className="footer">
-                  <Typography component="h5">
-                    Already have an account? <Link to="/login">Log In</Link>
-                  </Typography>
-                </Grid>
-              </Box>
-            </Form>
-          </Formik>
-        </Box>
+        <RegistrationForm />
       </Container>
     </>
   );
